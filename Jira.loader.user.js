94e5d9e1388054c941468de9fa02484ea49db246
Roy Froma <roy.froma@ictu.nl>
2026-04-15 08:50:42 +0200

    Bump loader version for update detection

--- Jira.loader.user.js
+++ Jira.loader.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Local Tampermonkey Bootstrap
 // @namespace    https://github.com/Meter-develop/jira-monkey/
-// @version      4.6
+// @version      4.7
 // @description  Manually installed trusted loader for local userscripts; manifest and script updates only load after local approval.
 // @match        *://*/*
 // @run-at       document-start
